6b8475737f25fde01ec4024991bbf01578c58e87
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-28 11:09:06 +0100

    refactor: 💡  isolated main section

--- js/fetchedData.js
+++ js/fetchedData.js
@@ -1,7 +1,6 @@
 async function fetchDataByPageNumber(pageNumber) {
   const RESPONSE = await fetch(`https://api.fbi.gov/wanted?page=${pageNumber}`);
   const data = await RESPONSE.json();
-  console.log(data);
   return data;
 }
 export { fetchDataByPageNumber };
