dcefc5043efeee6051060da6a93376d0d9cc7e14
DorotheeBM <braud.dorothee@gmail.com>
2024-11-29 14:37:01 +0100

    Revert "feat: 🎸  finished home page"
    This reverts commit fbcd60e0c83083c12d72995101ecc401f3bf2382.

--- script.js
+++ script.js
@@ -1,124 +1,55 @@
-// import dictionary from "./data/dictionary.js";
-import {
-  addHeaderComponent,
-  addMainComponent,
-  addPaginationComponent,
-} from "./js/Components.js";
+import dictionary from "./data/dictionary.js";
+import { addHeaderComponents } from "./js/Components.js";
 import { fetchDataByPageNumber } from "./js/fetchedData.js";
-import {
-  getHtmlElement,
-  displayHtmlElementContent,
-} from "./js/domManipulation.js";
-import { createNavLinks } from "./js/domManipulation.js";
-import { getAllHtmlElements } from "./js/domManipulation.js";
+import { addMainComponents } from "./js/Components.js"
 
-const HEADER = addHeaderComponent();
-const MAIN = addMainComponent();
-const PAGINATION = addPaginationComponent();
+const HEADER = addHeaderComponents();
+const MAIN = addMainComponents();
 
 document.addEventListener("DOMContentLoaded", () => {
-  const WEBSITE = getHtmlElement("#Website");
-
-  displayHtmlElementContent(
-    WEBSITE,
-    `
-    ${HEADER}    
-    ${MAIN}
-    ${PAGINATION}
+  const WEBSITE = document.querySelector("#Website");
+  WEBSITE.innerHTML = `
+  ${HEADER}    
+  ${MAIN}
     <footer>
-      contact du FBI: 
+    contact du FBI: 
     </footer>
-  `,
-  );
+    `;
 
-  let pageNumber = 1;
-  fetchDataByPageNumber(pageNumber).then((data) => {
-    const navList = getHtmlElement("#nav-list");
+  fetchDataByPageNumber(1).then((data) => {
+    console.log(data);
+    const navList = document.querySelector("#nav-list");
+    // console.log(navList)
 
-    createNavLinks(navList);
+    for (const key in dictionary) {
+      navList.innerHTML += `
+        <li>
+        <button type="button" class="nav-link">${dictionary[key]}</button>
+        </li>
+        `;
+    }
 
-    const BUTTONS_NAV_HTML = getAllHtmlElements(".nav-link");
-
-    BUTTONS_NAV_HTML.forEach((button) => {
+    const BUTTONS_HTML = document.querySelectorAll(".nav-link");
+    //WIP
+    BUTTONS_HTML.forEach((button) => {
       button.addEventListener("click", () => {
         const MAIN = document.querySelector("main");
-        displayHtmlElementContent(
-          MAIN,
-          `
-          <p>${button.textContent}</p>
-          `,
-        );
+        MAIN.innerHTML = `<p>${button.textContent}</p>`;
       });
     });
 
     const LOGO = document.querySelector("#logo-recherche button");
-    const htmlMain = document.querySelector("main");
     LOGO.addEventListener("click", () => {
-      htmlMain.innerHTML = "";
-      data.items.forEach((item) => {
-        htmlMain.innerHTML += `
+      const MAIN = document.querySelector("main");
+      MAIN.innerHTML = `
           <button type="button" class="thumbnail">
-          <div class="image-frame">
-          <img src="${item.images[0].large}" alt="NO IMAGE" />
-    </div>
-    <p>${item.title}</p>
-    </button>`;
-      });
-    });
-
-    htmlMain.innerHTML = "";
-    data.items.forEach((item) => {
-      htmlMain.innerHTML += `
-          <button type="button" class="thumbnail">
-          <div class="image-frame">
-          <img src="${item.images[0].large}" alt="NO IMAGE" />
-    </div>
-    <p>${item.title}</p>
+    <img src="" alt="NO IMAGE" />
+    <p></p>
     </button>
-    `;
-    });
-    const htmlPagination = document.querySelector("#pagination");
-    for (let i = 1; i < Math.ceil(data.total / data.items.length) + 1; i++) {
-      htmlPagination.innerHTML += `<button type="button">${i}</button>
-    `;
-    }
-
-    const paginationButtons = document.querySelectorAll("#pagination button");
-    paginationButtons.forEach((button) => {
-      button.addEventListener("click", () => {
-        pageNumber = parseInt(button.textContent);
-
-        fetchDataByPageNumber(pageNumber).then((data) => {
-          const LOGO = document.querySelector("#logo-recherche button");
-          const htmlMain = document.querySelector("main");
-          LOGO.addEventListener("click", () => {
-            htmlMain.innerHTML = "";
-            data.items.forEach((item) => {
-              htmlMain.innerHTML += `
-          <button type="button" class="thumbnail">
-          <div class="image-frame">
-          <img src="${item.images[0].large}" alt="NO IMAGE" />
-    </div>
-    <p>${item.title}</p>
-    </button>`;
-            });
-          });
-
-          htmlMain.innerHTML = "";
-          data.items.forEach((item) => {
-            htmlMain.innerHTML += `
-          <button type="button" class="thumbnail">
-          <div class="image-frame">
-          <img src="${item.images[0].large}" alt="NO IMAGE" />
-    </div>
-    <p>${item.title}</p>
-    </button>
-    `;
-          });
-        });
-      });
+    <section id="pagination"></section>`;
     });
 
+    
     //do not delete
   });
 });
