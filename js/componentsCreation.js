09c6b7c2cd0224d49d25d529d3cb49b916b6f77a
Ange230700 <kouakouangeericstephane@gmail.com>
2024-12-01 14:16:36 +0100

    refactor: 💡  defined the static HTML structure of the website

---
+++ js/componentsCreation.js
@@ -0,0 +1,34 @@
+// js\componentsCreation.js
+
+const HEADER = `
+  <header>
+    <section id="logo-recherche">
+      <button type="button">DAF</button>
+      <input type="search" id="site-search" placeholder="Search by ..."/>
+    </section>
+    <section id="description">
+      <h1>Ongoing FBI investigations</h1>
+    </section>
+    <nav id="filters"></nav>
+  </header>
+`;
+
+const MAIN = `
+  <main></main>
+`;
+
+const PAGINATION = `
+  <section id="pagination"></section>
+`;
+
+const FOOTER = `
+  <footer>
+    <section id="US_Embassy">
+      <p>
+        Contact de l'<a target="_blank" href="https://fr.usembassy.gov/fr/contact-fr/">Ambassade des Etats-unis d'Amérique à Paris</a> :   +33 (0)1 43 12 22 22
+      </p>
+    </section>
+  </footer>
+`;
+
+export { HEADER, MAIN, PAGINATION, FOOTER };
