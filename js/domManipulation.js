dffcbdcdd984498db2a9d42e02e6bd1f3237c842
Ange230700 <kouakouangeericstephane@gmail.com>
2024-12-01 14:23:22 +0100

    refactor: 💡  implemented functions for manipulating the dom

--- js/domManipulation.js
+++ js/domManipulation.js
@@ -1,44 +1,89 @@
-import dictionary from "../data/dictionary.js";
+// js\domManipulation.js
 
-// GENERAL
+import { handleClickOnPaginationButton } from "./eventsHandlers.js";
+import { globalVariables } from "./globalVariables.js";
 
-function getHtmlElement(cssSelector) {
-  return document.querySelector(cssSelector);
-}
-
-function getAllHtmlElements(cssSelector) {
-  return document.querySelectorAll(cssSelector);
-}
-
-function displayHtmlElementContent(htmlElement, content) {
+/* ———— GENERIC ———— */
+function initializeHtmlElementContent(htmlElement, content) {
   htmlElement.innerHTML = content;
 }
 
-function updateHtmlElementContent(htmlElement, content) {
+function emptyHtmlElementCurrentContent(htmlElement) {
+  htmlElement.innerHTML = "";
+}
+
+function fillHtmlElementWithNewContent(htmlElement, content) {
   htmlElement.innerHTML += content;
 }
 
-// HEADER
+/* ———— HEADER ———— */
+function createDropdownMenu(parentElement, labelText, optionsArray, variableName) {
+  const label = document.createElement("label");
+  label.textContent = labelText;
+  label.for = variableName;
+  
+  const select = document.createElement("select");
+  select.name = variableName;
+  select.id = variableName;
+  
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "";
+  defaultOption.textContent = "Any";
+  select.appendChild(defaultOption);
+  
+  optionsArray.forEach((optionValue) => {
+    const option = document.createElement("option");
+    option.value = optionValue;
+    option.textContent = optionValue;
+    select.appendChild(option);
+  });
+  
+  parentElement.appendChild(label);
+  parentElement.appendChild(select);
+}
 
-function createNavLinks(navList) {
-  for (const key in dictionary) {
-    updateHtmlElementContent(
-      navList,
-      `
-      <li>
-        <button type="button" class="nav-link">
-          ${dictionary[key]}
-        </button>
-      </li>
+const resetDropdownOptions = () => {
+  ["race", "eyes", "hair"].forEach((id) => {
+    document.querySelector(`#${id}`).value = "";
+  });
+};
+
+/* ———— MAIN ———— */
+function fillMainSectionWithThumbnails(item) {
+  fillHtmlElementWithNewContent(
+    document.querySelector("main"),
+    `
+    <button type="button" class="thumbnail">
+    <div class="image-frame">
+    <img src="${item.images[0]?.thumb}"  alt="NO IMAGE" loading="lazy" />
+    </div>
+    <p>${item.title}</p>
+    </button>
     `,
-    );
+  );
+}
+
+/* ———— PAGINATION ———— */
+function updatePaginationButtons(data) {
+  emptyHtmlElementCurrentContent(document.querySelector("#pagination"));
+
+  for (let i = 1; i < Math.ceil(data.total / globalVariables.pageSize) + 1; i++) {
+    fillHtmlElementWithNewContent(document.querySelector("#pagination"), `<button type="button">${i}</button>`);
   }
+
+  document.querySelectorAll("#pagination button").forEach((button) => {
+    button.addEventListener("click", (event) => {
+      handleClickOnPaginationButton(event);
+    });
+  });
 }
 
 export {
-  getHtmlElement,
-  getAllHtmlElements,
-  displayHtmlElementContent,
-  createNavLinks,
-  updateHtmlElementContent,
+  initializeHtmlElementContent,
+  emptyHtmlElementCurrentContent,
+  fillHtmlElementWithNewContent,
+  createDropdownMenu,
+  resetDropdownOptions,
+  fillMainSectionWithThumbnails,
+  updatePaginationButtons,
 };
