a07ffe1b6de9f52eea62e28e6c6b576309d4698f
Ange230700 <kouakouangeericstephane@gmail.com>
2024-12-03 00:18:03 +0100

    refactor: 💡  refactored the code some more

--- js/componentsCreation.js
+++ js/componentsCreation.js
@@ -15,22 +15,35 @@ const HEADER = `
   </header>
 `;
 
+/* ———— MAIN ———— */
 const MAIN = `
-  <main></main>
+  <main>
+    <section id="thumbnails"></section>
+  </main>
+`;
+
+const THUMBNAIL = `
+  <button type="button" class="thumbnail">
+    <div class="image-frame">
+      <img src="" alt="NO IMAGE" loading="lazy" />
+    </div>
+    <p></p>
+  </button>
 `;
 
 const PAGINATION = `
-  <section id="pagination"></section>
+<div id="pagination-container">
+<section id="pagination"></section>
+</div>
 `;
+/* ——————————— */
 
 const FOOTER = `
   <footer>
     <section id="US_Embassy">
-      <p>
-        Contact the <a target="_blank" href="https://fr.usembassy.gov/fr/contact-fr/">Embassy of the United States of America</a> in Paris : +33 (0)1 43 12 22 22
-      </p>
+      <p>Contact the <a target="_blank" href="https://fr.usembassy.gov/fr/contact-fr/">Embassy of the United States of America</a> in Paris : +33 (0)1 43 12 22 22</p>
     </section>
   </footer>
 `;
 
-export { HEADER, MAIN, PAGINATION, FOOTER };
+export { HEADER, MAIN, THUMBNAIL, PAGINATION, FOOTER };
